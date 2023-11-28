a6474bbdc77e87477ee2dcf562c8ed23239ba080
rjfuhrman42 <reidjf77@gmail.com>
2023-11-28 15:14:07 -0500

    feat: add in colors, fonts, remove placeholder content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "darkest-blue": "#002B36",
+        "dark-blue": "#073642",
+        "darkest-gray": "#586E75",
+        "dark-gray": "#586E75",
+        "light-gray": "#839496",
+        "lighter-gray": "#93A1A1",
+        "tan": "#EEE8D5",
+        "light-tan": "#FDF6E3",
+        "accent-yellow": "#FFCC33",
+        "accent-orange": "#E96B35",
+        "accent-red": "#DE423F",
+        "accent-pink": "#F91090",
+        "accent-purple": "#9465B8",
+        "accent-blue": "#53A5DF",
+        "accent-teal": "#6CDAD1",
+        "accent-green": "#C2E000",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
