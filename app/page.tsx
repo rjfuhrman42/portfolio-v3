afb8456324937350d1e31fd543b9ba97d72d9558
rjfuhrman42 <reidjf77@gmail.com>
2023-11-28 16:20:46 -0500

    feat: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-darkest-blue">
-      <div className="flex flex-col items-center justify-center text-accent-green font-heading-tall">
-        hello world!
+    <main className="flex min-h-screen flex-col items-center justify-between bg-darkest-blue">
+      <div className="max-w-xl py-16 flex flex-col items-center justify-center">
+        <div className="w-full">
+          <div className="pb-11">
+            <h1 className="text-accent-green">Hi, I&apos;m Reid.</h1>
+            <h1 className="">A Software engineer based in new york.</h1>
+          </div>
+          <p className="">
+            I’m Reid Fuhrman, a software engineer based in Brooklyn, New York.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            vestibulum arcu id mauris ullamcorper maximus. Nam consequat odio eu
+            sollicitudin porttitor. Pellentesque maximus eget est vitae tempus.
+            Ut sodales ipsum quis urna malesuada, in vehicula lectus pretium.
+            Sed faucibus aliquet dui ac vehicula. In sed justo risus.
+          </p>
+        </div>
       </div>
     </main>
   );
