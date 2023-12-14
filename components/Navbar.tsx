7ebf60ac7ae22e6369f6246598af71248bafe873
rjfuhrman42 <reidjf77@gmail.com>
2023-12-14 14:22:14 -0500

    feat: add fun home button animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { stagger, useAnimate } from "framer-motion";
 
 interface NavItem {
   href: string;
@@ -14,17 +16,30 @@ const navItems: NavItem[] = [
 ];
 
 const Navbar = () => {
+  const [scope, animate] = useAnimate();
+
+  const onButtonClick = () => {
+    animate([
+      [".home-button", { y: [0, 2, 0] }, { duration: 0.25 }],
+      [
+        ".bubbles",
+        { y: -100, opacity: [0, 1, 0] },
+        { duration: 1, delay: stagger(0.1) },
+      ],
+      [".bubbles", { y: 0, opacity: 0 }, { duration: 0.00003 }],
+    ]);
+  };
   return (
     <div className="relative w-full flex justify-end items-center sm:gap-x-1">
-      <Link
-        href="/"
-        className="grid grid-cols-2 absolute left-0 py-4 pr-2 home-button"
-      >
-        <div className="bg-accent-green home-square"></div>
-        <div className="bg-accent-teal home-square"></div>
-        <div className="bg-accent-pink home-square"></div>
-        <div className="bg-accent-yellow home-square"></div>
+      <Link href="/" onClick={onButtonClick} className="absolute -left-4 p-4">
+        <div ref={scope}>
+          <p className="home-button font-heading">RF</p>
+          <div className="bubbles left-4" />
+          <div className="bubbles right-3" />
+          <div className="bubbles right-6" />
+        </div>
       </Link>
+
       {navItems.map((item) => {
         return (
           <Link
